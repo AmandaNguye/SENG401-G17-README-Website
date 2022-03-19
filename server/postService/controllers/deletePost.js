0a40b74cf6d035730a6f49372c0d25bbbbee920c
Dat Lam <lamdat2551@gmail.com>
2022-03-19 17:16:13 -0600

    Update delete controller

--- server/postService/controllers/deletePost.js
+++ server/postService/controllers/deletePost.js
@@ -1,9 +1,21 @@
 import Post from "../models/post.js";
 
 export const deletePost = async (req, res) => {
+	const userID = req.body.userID;
     try {
-		await Post.deleteOne({ _id: req.params.id });
-		res.status(204).send()
+		const result = await Post.deleteOne({ 
+			_id: req.params.id,
+			creator: userID
+		});
+		if(result.deletedCount == 1)
+		{
+			res.status(204).send();
+		}
+		else
+		{
+			res.status(400).send();
+		}
+
 	} catch {
 		res.status(404).json({ error: "Post doesn't exist!"});
 	}
