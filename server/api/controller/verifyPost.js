c68def7e60bff8bf7cc3d6e34fe8db64b953c753
Dat Lam <lamdat2551@gmail.com>
2022-03-24 16:07:09 -0600

    Create Post Routes in API gateway

---
+++ server/api/controller/verifyPost.js
@@ -0,0 +1,15 @@
+import got from "got";
+
+const postURL = process.env.POST_URL;
+
+
+export const verifyPost = async (req, res, next) => {
+    try {
+
+        const post = await got.get(postURL + "/" + req.params.p_id).json();
+        next();
+    }
+    catch (err) {
+        res.status(404).json({ error: "Post doesn't exist!" });
+    }
+}
--- server/api/index.js
+++ server/api/index.js
@@ -4,7 +4,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 import userRouter from "./router/userRoutes.js"
-import postRouter from "./router/postRouter.js"
+import postRouter from "./router/postRoutes.js"
+import commentRouter from "./router/commentRoutes.js"
 
 const app = express();
 
@@ -14,7 +15,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: "true" }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: "true" }));
 app.use(cors());
 
-
+app.use('/posts', commentRouter);
 app.use('/posts', postRouter);
 app.use('/', userRouter);
 
