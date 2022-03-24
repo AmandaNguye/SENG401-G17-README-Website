6223eecd202b6b95f797de8e35d66f7bb852e0ce
Dat Lam <lamdat2551@gmail.com>
2022-03-24 15:55:20 -0600

    Minor bug fix

--- server/api/router/postRoutes.js
+++ server/api/router/postRoutes.js
@@ -12,13 +12,14 @@ const postURL = process.env.POST_URL;
 router.get("/", async (req, res, next) => {
     try {
         const { page = 0, limit = 10, q } = req.query;
+        let url;
         if (q) {
-            const url = postURL + "/?page=" + page
+            url = postURL + "/?page=" + page
                 + "&limit=" + limit
                 + "&q=" + q;
         }
         else {
-            const url = postURL + "/?page=" + page + "&limit=" + limit;
+            url = postURL + "/?page=" + page + "&limit=" + limit;
         }
         const response = await got.get(url + "/", {
             headers: {
@@ -36,7 +37,6 @@ router.get("/user/:name", async (req, res, next) => {
     try {
         const { page = 0, limit = 10 } = req.query;
         const url = postURL + "/user/?page=" + page + "&limit=" + limit;
-
         const response = await got.get(url, {
             headers: {
                 "x-access-token": req.headers["x-access-token"]
