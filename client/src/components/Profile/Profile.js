55720dbff4d500a56b118e1355f242115b23fc5c
Viet An Truong <vietan124@gmail.com>
2022-03-24 22:16:47 -0600

    updating branch

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import PostList from "../PostList/PostList";
 
 const Profile = () => {
   const navigate = useNavigate();
@@ -14,14 +15,60 @@ const Profile = () => {
     })
       .then((res) => res.json())
       .then((data) =>
-        data.isLoggedIn ? setUsername(data.username) : navigate("/login")
+        data.isLoggedIn ? setUsername(data.username) : navigate("/dashboard")
       );
   });
 
+  const [posts, setPosts] = useState([]);
+
+  const handleLogout = (e) => {
+    e.preventDefault();
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
+
+  const handleDashboard = (e) => {
+    e.preventDefault();
+    navigate("/dashboard");
+  };
+
+  const loadPosts = async () => {
+    const payload = {
+      method: "GET",
+      headers: {
+        "x-access-token": localStorage.getItem("token"),
+      },
+    };
+
+    try {
+      const res = await fetch(
+        `http://localhost:5005/posts/user/${username}`,
+        payload
+      ); // Port 5001 for postService
+      const posts = await res.json();
+      console.log(posts);
+      setPosts(posts);
+      // console.log(posts);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    loadPosts();
+  }, []);
+
   return (
     <div>
-      <h2>Profile</h2>
-      <p>Your Username is {username}</p>
+      <button type="button" onClick={(e) => handleLogout(e)}>
+        Log Out
+      </button>
+      <button type="button" onClick={(e) => handleDashboard(e)}>
+        Dashboard
+      </button>
+      <h1>Welcome, {username} </h1>
+      <h2>Your Posts:</h2>
+      <PostList posts={posts} refreshPosts={loadPosts} />
     </div>
   );
 };
