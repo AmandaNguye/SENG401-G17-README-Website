b3181bd77e0378090626b1daf68226ee45a01113
ttran1017 <tylertran1017@gmail.com>
2022-03-25 04:51:18 -0600

    Post page styling fixes

--- client/src/components/Contexts/PostContext.js
+++ client/src/components/Contexts/PostContext.js
@@ -6,14 +6,39 @@ export const PostProvider = (props) => {
 	const [posts, setPosts] = useState([
 		{
 			_id: 1,
-			title: "Lorem ipsum dolor sit amet.",
+			title: "How I met your dad",
 			content:
-				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis non, corrupti culpa magnam atque facilis?",
-			tag: "meme",
-			fame_count: 1,
+				"He said he was buying milk and cigarettes. I had the feeling he wasn't buying milk and cigeratcigarettestes.",
+			tag: "MeIRL",
+			fame_count: 101,
+			lame_count: 1,
 			famer: [],
 			lamer: [],
-			creator: 1,
+			creator: "Yyccccccccccccchad",
+		},
+		{
+			_id: 2,
+			title: "Questions regarding poor eyesight",
+			content:
+				"Does using a computer for extended periods of time make you blind as a bat? It probably does doesn't it? Eventually I'll become Batman!",
+			tag: "Dazed and Confused",
+			fame_count: 69,
+			lame_count: 69,
+			famer: [],
+			lamer: [],
+			creator: "Robin",
+		},
+		{
+			_id: 3,
+			title: "How to find an element easily!",
+			content:
+				"arr.find( e => (condition involving e)), this will return the first item which meets your condition!",
+			tag: "Actually helpful",
+			fame_count: 2,
+			lame_count: 420,
+			famer: [],
+			lamer: [],
+			creator: "FacebookDev",
 		},
 	]);
 
