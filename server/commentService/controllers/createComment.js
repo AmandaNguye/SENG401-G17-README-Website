6977adbae7594468710d1a2d0036b7a006de49e9
ttran1017 <tylertran1017@gmail.com>
2022-03-25 17:39:18 -0600

    Fixes to controller

--- server/commentService/controllers/createComment.js
+++ server/commentService/controllers/createComment.js
@@ -1,27 +1,26 @@
 import Comment from "../models/comment.js";
 
-
 /**
  * Create comment using the information embedded in the http request
  * @param {*} req
- * 1. content: comment content 
- * 
- * @param {*} res 
+ * 1. content: comment content
+ *
+ * @param {*} res
  */
 export const createComment = async (req, res) => {
-    try {
-        const commentInfo = req.body;
-        const post = new Comment({
-            content: commentInfo.content,
-            fame_count: 0,
-            post: req.params.p_id,
-            famer: [],
-            lamer: [],
-            creator: req.user.username,
-        });
-        var result = await post.save();
-        res.json({ result: result });
-    } catch (e) {
-        res.status(406).json({ message: e });
-    }
-}
+	try {
+		const commentInfo = req.body;
+		const post = new Comment({
+			content: commentInfo.content,
+			fame_count: 0,
+			post: req.params.p_id,
+			famer: [],
+			lamer: [],
+			creator: req.user.username,
+		});
+		var result = await post.save();
+		res.json({ result: result });
+	} catch (e) {
+		res.status(406).json({ message: e });
+	}
+};
