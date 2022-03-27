2e2dd617c32fdc959601c337106eeeaec0fcab50
Viet An Truong <56201575+ItsMarcoMSF@users.noreply.github.com>
2022-03-26 18:35:01 -0600

    added Page navigation & conditional rendering of posts (must be logged in to render) (#24)
    * config frontend to work with api gateway
    
    * added script to start all servers and client at once
    
    * added Page navigation & conditional rendering of posts (user must be logged in for posts to render)

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -4,77 +4,105 @@ import { useNavigate } from "react-router-dom";
 import PostList from "../PostList/PostList";
 import PostForm from "../PostForm/PostForm";
 import CondensedPostForm from "../CondensedPostForm/CondensedPostForm";
+import PageSelector from "../PageSelector/PageSelector";
 
 import "./Dashboard.css";
 
 const Dashboard = () => {
-	const [posts, setPosts] = useState([]);
-	const navigate = useNavigate();
+  const navigate = useNavigate();
+  const [posts, setPosts] = useState([]);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [currPage, setCurrPage] = useState(0);
 
-	const [posting, setPosting] = useState(false);
+  const [posting, setPosting] = useState(false);
 
-	useEffect(() => {
-		fetch("http://localhost:5005/isUserAuth", {
-			headers: {
-				"x-access-token": localStorage.getItem("token"),
-			},
-		})
-			.then((res) => res.json())
-			.then((data) =>
-				data.isLoggedIn ? console.log(data.username) : navigate("/login")
-			);
-	});
+  useEffect(() => {
+    fetch("http://localhost:5005/isUserAuth", {
+      headers: {
+        "x-access-token": localStorage.getItem("token"),
+      },
+    })
+      .then((res) => res.json())
+      .then((data) =>
+        data.isLoggedIn ? setIsLoggedIn(true) : navigate("/login")
+      );
+  });
 
-	const handleLogout = (e) => {
-		e.preventDefault();
-		localStorage.removeItem("token");
-		navigate("/login");
-	};
+  const handleLogout = (e) => {
+    e.preventDefault();
+    localStorage.removeItem("token");
+    setIsLoggedIn(false);
+    navigate("/login");
+  };
 
-	const handleProfile = (e) => {
-		e.preventDefault();
-		navigate("/profile");
-	};
+  const handleProfile = (e) => {
+    e.preventDefault();
+    navigate("/profile");
+  };
 
-	const loadPosts = async () => {
-		const payload = {
-			method: "GET",
-			headers: {
-				"x-access-token": localStorage.getItem("token"),
-			},
-		};
+  const nextPage = (e) => {
+    e.preventDefault();
+    setCurrPage(currPage + 1);
+  };
 
-		try {
-			const res = await fetch(`http://localhost:5005/posts`, payload);
-			const posts = await res.json();
-			setPosts(posts);
-			// console.log(posts);
-		} catch (err) {
-			console.error(err);
-		}
-	};
+  const prevPage = (e) => {
+    e.preventDefault();
+    setCurrPage(currPage - 1);
+  };
 
-	useEffect(() => {
-		loadPosts();
-	}, []);
+  const loadPosts = async () => {
+    const payload = {
+      method: "GET",
+      headers: {
+        "x-access-token": localStorage.getItem("token"),
+      },
+    };
 
-	return (
-		<div className="dashboard-wrapper">
-			<h2>Dashboard</h2>
-			<button type="button" onClick={(e) => handleLogout(e)}>
-				Log Out
-			</button>
-			<button type="button" onClick={(e) => handleProfile(e)}>
-				Profile
-			</button>
-			{posting ? (
-				<PostForm refreshPosts={loadPosts} setForm={setPosting} />
-			) : (
-				<CondensedPostForm setForm={setPosting} />
-			)}
-			<PostList posts={posts} refreshPosts={loadPosts} />
-		</div>
-	);
+    try {
+      const res = await fetch(
+        `http://localhost:5005/posts?page=${currPage}`,
+        payload
+      );
+      const posts = await res.json();
+      setPosts(posts);
+      // console.log(posts);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    loadPosts();
+  }, [currPage]);
+
+  return (
+    isLoggedIn && (
+      <div className="dashboard-wrapper">
+        <h2>Dashboard</h2>
+        <button type="button" onClick={(e) => handleLogout(e)}>
+          Log Out
+        </button>
+        <button type="button" onClick={(e) => handleProfile(e)}>
+          Profile
+        </button>
+        {posting ? (
+          <PostForm refreshPosts={loadPosts} setForm={setPosting} />
+        ) : (
+          <CondensedPostForm setForm={setPosting} />
+        )}
+        <PostList posts={posts} refreshPosts={loadPosts} />
+
+        <div className="dashboard-footer">
+          <PageSelector
+            pageNo={currPage}
+            nextPage={nextPage}
+            prevPage={prevPage}
+            hasPosts={posts.length > 0}
+          />
+        </div>
+      </div>
+    )
+  );
 };
 
 export default Dashboard;
--- client/src/components/PostCard/PostCard.js
+++ client/src/components/PostCard/PostCard.js
@@ -6,162 +6,162 @@ import { TriangleUpIcon, TriangleDownIcon } from "@chakra-ui/icons";
 import "./PostCard.css";
 
 const PostCard = ({ post, refreshPosts }) => {
-	const navigate = useNavigate(); // for title link, comment link, etc.
-	const famed = post.famed;
-	const lamed = post.lamed;
+  const navigate = useNavigate(); // for title link, comment link, etc.
+  const famed = post.famed;
+  const lamed = post.lamed;
 
-	const size = 20;
-	const upvoteColor = "#644aff";
-	const downvoteColor = "#ffd25e";
+  const size = 20;
+  const upvoteColor = "#644aff";
+  const downvoteColor = "#ffd25e";
 
-	const token = localStorage.getItem("token");
+  const token = localStorage.getItem("token");
 
-	const cancelVote = async (e) => {
-		e.preventDefault();
-		const postID = post._id;
-		const data = {
-			voteType: "",
-		};
-		const payload = {
-			method: "PATCH",
-			headers: {
-				Accept: "application/json",
-				"Content-Type": "application/json",
-				"x-access-token": token,
-			},
-			body: JSON.stringify(data),
-		};
-		try {
-			const res = await fetch(
-				`http://localhost:5005/posts/${postID}/vote`,
-				payload
-			);
-			if (res.ok) {
-				refreshPosts();
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	};
+  const cancelVote = async (e) => {
+    e.preventDefault();
+    const postID = post._id;
+    const data = {
+      voteType: "",
+    };
+    const payload = {
+      method: "PATCH",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "x-access-token": token,
+      },
+      body: JSON.stringify(data),
+    };
+    try {
+      const res = await fetch(
+        `http://localhost:5005/posts/${postID}/vote`,
+        payload
+      );
+      if (res.ok) {
+        refreshPosts();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-	const upvote = async (e) => {
-		if (famed) {
-			cancelVote(e);
-			return;
-		}
+  const upvote = async (e) => {
+    if (famed) {
+      cancelVote(e);
+      return;
+    }
 
-		e.preventDefault();
-		const postID = post._id;
-		const data = {
-			voteType: "fame",
-		};
-		const payload = {
-			method: "PATCH",
-			headers: {
-				Accept: "application/json",
-				"Content-Type": "application/json",
-				"x-access-token": token,
-			},
-			body: JSON.stringify(data),
-		};
-		try {
-			const res = await fetch(
-				`http://localhost:5005/posts/${postID}/vote`,
-				payload
-			);
-			if (res.ok) {
-				refreshPosts();
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	};
+    e.preventDefault();
+    const postID = post._id;
+    const data = {
+      voteType: "fame",
+    };
+    const payload = {
+      method: "PATCH",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "x-access-token": token,
+      },
+      body: JSON.stringify(data),
+    };
+    try {
+      const res = await fetch(
+        `http://localhost:5005/posts/${postID}/vote`,
+        payload
+      );
+      if (res.ok) {
+        refreshPosts();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-	const downvote = async (e) => {
-		if (lamed) {
-			cancelVote(e);
-			return;
-		}
+  const downvote = async (e) => {
+    if (lamed) {
+      cancelVote(e);
+      return;
+    }
 
-		e.preventDefault();
-		const postID = post._id;
-		const data = {
-			voteType: "lame",
-		};
-		const payload = {
-			method: "PATCH",
-			headers: {
-				Accept: "application/json",
-				"Content-Type": "application/json",
-				"x-access-token": token,
-			},
-			body: JSON.stringify(data),
-		};
-		try {
-			const res = await fetch(
-				`http://localhost:5005/posts/${postID}/vote`,
-				payload
-			);
-			if (res.ok) {
-				refreshPosts();
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	};
+    e.preventDefault();
+    const postID = post._id;
+    const data = {
+      voteType: "lame",
+    };
+    const payload = {
+      method: "PATCH",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "x-access-token": token,
+      },
+      body: JSON.stringify(data),
+    };
+    try {
+      const res = await fetch(
+        `http://localhost:5005/posts/${postID}/vote`,
+        payload
+      );
+      if (res.ok) {
+        refreshPosts();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-	const UpvoteIcon = (
-		<TriangleUpIcon w={size} h={size} _hover={{ color: upvoteColor }} />
-	);
+  const UpvoteIcon = (
+    <TriangleUpIcon w={size} h={size} _hover={{ color: upvoteColor }} />
+  );
 
-	const DownvoteIcon = (
-		<TriangleDownIcon w={size} h={size} _hover={{ color: downvoteColor }} />
-	);
+  const DownvoteIcon = (
+    <TriangleDownIcon w={size} h={size} _hover={{ color: downvoteColor }} />
+  );
 
-	return (
-		<div className="container">
-			<div className="post-card">
-				<div className="vote-box">
-					<IconButton
-						role="group"
-						onClick={upvote}
-						backgroundColor="transparent"
-						color={famed ? upvoteColor : "gray"}
-						boxShadow="none !important"
-						border="none"
-						cursor="pointer"
-						icon={UpvoteIcon}
-					/>
-					<p>{post.fame_count}</p>
-					<IconButton
-						role="group"
-						onClick={downvote}
-						backgroundColor="transparent"
-						color={lamed ? downvoteColor : "gray"}
-						boxShadow="none !important"
-						border="none"
-						cursor="pointer"
-						icon={DownvoteIcon}
-					/>
-				</div>
-				<div className="post-content">
-					<a
-						className="title"
-						onClick={(e) => {
-							e.preventDefault();
-							console.log(`post ${post._id} clicked`);
-							navigate(`/post-page/${post._id}`);
-						}}
-					>
-						{post.title}
-					</a>
-					<p className="post-text">{post.content}</p>
+  return (
+    <div className="card-container">
+      <div className="post-card">
+        <div className="post-vote-box">
+          <IconButton
+            role="group"
+            onClick={upvote}
+            backgroundColor="transparent"
+            color={famed ? upvoteColor : "gray"}
+            boxShadow="none !important"
+            border="none"
+            cursor="pointer"
+            icon={UpvoteIcon}
+          />
+          <p>{post.fame_count}</p>
+          <IconButton
+            role="group"
+            onClick={downvote}
+            backgroundColor="transparent"
+            color={lamed ? downvoteColor : "gray"}
+            boxShadow="none !important"
+            border="none"
+            cursor="pointer"
+            icon={DownvoteIcon}
+          />
+        </div>
+        <div className="post-content">
+          <a
+            className="title"
+            onClick={(e) => {
+              e.preventDefault();
+              console.log(`post ${post._id} clicked`);
+              navigate(`/post-page/${post._id}`);
+            }}
+          >
+            {post.title}
+          </a>
+          <p className="post-text">{post.content}</p>
 
-					<div className="card-footer"></div>
-				</div>
-			</div>
-		</div>
-	);
+          <div className="card-footer"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PostCard;
