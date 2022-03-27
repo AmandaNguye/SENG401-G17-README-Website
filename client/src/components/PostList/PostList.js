e5a2dae1181d974c69ff68831efc1e0b785f8f3c
Viet An Truong <56201575+ItsMarcoMSF@users.noreply.github.com>
2022-03-26 18:35:01 -0600

    added Page navigation & conditional rendering of posts (must be logged in to render) (#24)
    * config frontend to work with api gateway
    
    * added script to start all servers and client at once
    
    * added Page navigation & conditional rendering of posts (user must be logged in for posts to render)

--- client/src/components/PostList/PostList.js
+++ client/src/components/PostList/PostList.js
@@ -4,12 +4,18 @@ import PostCard from "../PostCard/PostCard";
 import "./PostList.css";
 
 const PostList = ({ posts, refreshPosts }) => {
+  const hasPosts = posts.length > 0;
   return (
     <div className="post-list">
-      {posts &&
+      {hasPosts ? (
         posts.map((post) => (
           <PostCard key={post._id} post={post} refreshPosts={refreshPosts} />
-        ))}
+        ))
+      ) : (
+        <p className="no-posts">
+          Nothing to see here, just an empty void, like my soul
+        </p>
+      )}
     </div>
   );
 };
